37dbe6d5b6e55dc0ed10b59d70e8919962384edf
elias el shee5 <elias.musharbash11@gmail.com>
2023-11-16 18:23:09 +0300

    add files of html and css

---
+++ public/interface.js
@@ -0,0 +1,21 @@
+const login = document.querySelector('.login');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.login-register');
+const headerloginIcon = document.querySelector('.headerlogin-icon');
+const closeLogin = document.querySelector('.close-login');
+
+registerLink.addEventListener('click', ()=> {
+    login.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    login.classList.remove('active');
+});
+
+headerloginIcon.addEventListener('click', ()=> {
+    login.classList.add('active-popup');
+});
+
+closeLogin.addEventListener('click', ()=> {
+    login.classList.remove('active-popup');
+});
